e3ee72b6dd89089e77be963db6bc7c7aa2af0a0d
Tao Watanabe <momossy5@gmail.com>
2020-08-27 16:36:57 +0900

    viewModel定義と呼び出し

---
+++ app/src/main/java/com/example/intern_3days_hackathon/view/SavedEventViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.intern_3days_hackathon.view
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.intern_3days_hackathon.data.SavedEvent
+import com.example.intern_3days_hackathon.data.SavedEventDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class SavedEventViewModel(
+        private val savedEventDao: SavedEventDao,
+        application: Application) : AndroidViewModel(application) {
+
+    val eventList: LiveData<List<SavedEvent>> = savedEventDao.getAllEvent()
+
+    fun insertEvent(event: SavedEvent) = viewModelScope.launch(Dispatchers.IO) {
+        savedEventDao.insert(event)
+    }
+
+}
