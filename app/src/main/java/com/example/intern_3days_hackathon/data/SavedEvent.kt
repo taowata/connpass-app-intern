8e6e1cd7204165559b2f15e097ac6cb5b103ba72
Tao Watanabe <momossy5@gmail.com>
2020-08-27 15:03:55 +0900

    依存関係追加 エンティティクラス作成

---
+++ app/src/main/java/com/example/intern_3days_hackathon/data/SavedEvent.kt
@@ -0,0 +1,16 @@
+package com.example.intern_3days_hackathon.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "saved_event_table")
+data class SavedEvent(
+        @ColumnInfo(name = "title") var title: String,
+        @ColumnInfo(name = "date") var date: String,
+        @ColumnInfo(name = "location") var location: String,
+        @ColumnInfo(name = "event_url") var url: String,
+        @ColumnInfo(name = "has_finished") var hasFinished: Boolean,
+        @ColumnInfo(name = "image_url") var imageUrl: String,
+        @PrimaryKey(autoGenerate = true) val eventId: Int = 0
+)
