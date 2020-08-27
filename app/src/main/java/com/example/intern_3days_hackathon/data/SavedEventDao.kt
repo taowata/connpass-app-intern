cd15d28c813126288a0e0b61172fffc896446e8d
Tao Watanabe <momossy5@gmail.com>
2020-08-28 07:50:48 +0900

    イベントの削除機能実装、アプリテーマをマテリアルデザインに変更

--- app/src/main/java/com/example/intern_3days_hackathon/data/SavedEventDao.kt
+++ app/src/main/java/com/example/intern_3days_hackathon/data/SavedEventDao.kt
@@ -2,6 +2,7 @@ package com.example.intern_3days_hackathon.data
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 
@@ -9,6 +10,9 @@ import androidx.room.Query
 interface SavedEventDao {
     @Insert
     fun insert(event: SavedEvent)
+
+    @Delete
+    fun delete(event: SavedEvent)
 
     @Query("select * from saved_event_table order by date asc")
     fun getAllEvent(): LiveData<List<SavedEvent>>
