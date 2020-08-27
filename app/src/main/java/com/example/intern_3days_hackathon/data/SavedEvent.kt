335dc624c2db8e8c1096a99f8be37a3d3f8c23b8
Tao Watanabe <momossy5@gmail.com>
2020-08-27 15:26:34 +0900

    dao定義

--- app/src/main/java/com/example/intern_3days_hackathon/data/SavedEvent.kt
+++ app/src/main/java/com/example/intern_3days_hackathon/data/SavedEvent.kt
@@ -6,11 +6,11 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "saved_event_table")
 data class SavedEvent(
-        @ColumnInfo(name = "title") var title: String,
-        @ColumnInfo(name = "date") var date: String,
-        @ColumnInfo(name = "location") var location: String,
-        @ColumnInfo(name = "event_url") var url: String,
-        @ColumnInfo(name = "has_finished") var hasFinished: Boolean,
-        @ColumnInfo(name = "image_url") var imageUrl: String,
+        @ColumnInfo(name = "title") var title: String = "",
+        @ColumnInfo(name = "date") var date: String = "",
+        @ColumnInfo(name = "location") var location: String = "",
+        @ColumnInfo(name = "event_url") var url: String = "",
+        @ColumnInfo(name = "has_finished") var hasFinished: Boolean = false,
+        @ColumnInfo(name = "image_url") var imageUrl: String = "",
         @PrimaryKey(autoGenerate = true) val eventId: Int = 0
 )
---
+++ app/src/main/java/com/example/intern_3days_hackathon/data/SavedEventDao.kt
@@ -0,0 +1,16 @@
+package com.example.intern_3days_hackathon.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface SavedEventDao {
+    @Insert
+    suspend fun insert(event: SavedEvent)
+
+    @Query("select * from saved_event_table order by date asc")
+    fun getAllEvent(): LiveData<List<SavedEvent>>
+
+}
