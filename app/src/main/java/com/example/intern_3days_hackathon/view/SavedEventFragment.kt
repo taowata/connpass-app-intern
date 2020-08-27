84d892b37d64361b7945c74518cf8451c9b337b9
Tao Watanabe <momossy5@gmail.com>
2020-08-27 17:04:37 +0900

    イベント名のみ指定してDBに保存

--- app/src/main/java/com/example/intern_3days_hackathon/view/SavedEventFragment.kt
+++ app/src/main/java/com/example/intern_3days_hackathon/view/SavedEventFragment.kt
@@ -1,12 +1,17 @@
 package com.example.intern_3days_hackathon.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.lifecycle.ViewModelProvider
 import com.example.intern_3days_hackathon.R
+import com.example.intern_3days_hackathon.data.EventDatabase
+import com.example.intern_3days_hackathon.data.SavedEvent
 
 class SavedEventFragment : Fragment() {
 
@@ -21,6 +26,23 @@ class SavedEventFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        savedEventViewModel = ViewModelProvider(this).get(SavedEventViewModel::class.java)
+        val application = requireNotNull(this.activity).application
+        val dataSource = EventDatabase.getInstance(application).savedEventDao
+
+        // val viewModelFactory = SleepTrackerViewModelFactory(dataSource, application)
+        val viewModelFactory = SavedEventViewModelFactory(dataSource, application)
+
+        savedEventViewModel = ViewModelProvider(this, viewModelFactory).get(SavedEventViewModel::class.java)
+
+
+        val inputButton = view.findViewById<Button>(R.id.input_event_button)
+
+        inputButton.setOnClickListener {
+            val eventName = view.findViewById<EditText>(R.id.event_name_edit).text.toString()
+            val newEvent = SavedEvent(title = eventName)
+            savedEventViewModel.insertEvent(newEvent)
+            Log.i("test", newEvent.title)
+        }
+
     }
 }
